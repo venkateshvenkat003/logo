95cd00986c57eeed2657397060a5a2b2d7e535e4
venkateshvenkat003 <lightingshadow003@gmail.com>
2024-04-09 18:57:23 +0530

    new files

---
+++ node.js
@@ -0,0 +1,26 @@
+// const numbers =[1,2,3,4,5,6,7]
+
+// let sq=numbers.map((values)=>{
+//     return Math.sqrt(values);
+// });
+
+// console.table(sq)
+
+
+// const users=[
+//     {name:"ram", age:15, city:"chennai", salary:15000 },
+//     {name:"ramesh", age:23, city:"chennai", salary:15000 },
+//     {name:"ram", age:45, city:"chennai", salary:15000 },
+//     {name:"ram", age:12, city:"chennai", salary:15000 },
+//     {name:"ram", age:90, city:"chennai", salary:15000 },
+
+// ];
+
+// let eligible = users.map((user)=>
+// ({
+//     ...user,
+//     status:user.age>=18?"eligible":"not eligible"
+// }));
+
+// console.table(eligible)
+
